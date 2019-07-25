6e9fd569098b30be6a181a482af5b9e7390853ad
Koen Vendrik <k.vendrik@gmail.com>
2019-07-25 16:40:03 -0400

    adds examples sidebar

--- website/pages/en/playground.js
+++ website/pages/en/playground.js
@@ -7,27 +7,43 @@
 
 const React = require('react');
 
-const CompLibrary = require('../../core/CompLibrary.js');
-const Container = CompLibrary.Container;
-const GridBlock = CompLibrary.GridBlock;
+const REPL_EXAMPLE_URLS = {
+  basic: {
+    name: 'Basic',
+    url: 'https://repl.it/@amasad/try-jest',
+  },
+};
 
-const translate = require('../../server/translate.js').translate;
-
-function Playground() {
+function Playground(props) {
+  const exampleId = 'basic';
+  const exampleNavItems = Object.keys(REPL_EXAMPLE_URLS).map(id => (
+    <li key={id}>
+      <a href={`?example_id=${encodeURI(id)}`}>{REPL_EXAMPLE_URLS[id].name}</a>
+    </li>
+  ));
   return (
-    <div>
+    <section className="playground">
+      <aside className="playground__sidebar">
+        <h4 className="playground__title">Examples</h4>
+        <ul className="playground__nav">{exampleNavItems}</ul>
+      </aside>
       <iframe
         height="700px"
         width="100%"
-        src="https://repl.it/@amasad/try-jest?lite=true"
+        src={`${getExampleUrlById(exampleId)}?lite=true`}
         scrolling="no"
         frameborder="no"
         allowtransparency="true"
         allowfullscreen="true"
         sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"
       ></iframe>
-    </div>
+    </section>
   );
+}
+
+function getExampleUrlById(id) {
+  const details = REPL_EXAMPLE_URLS[id];
+  return details ? details.url : REPL_EXAMPLE_URLS.basic.url;
 }
 
 module.exports = Playground;
